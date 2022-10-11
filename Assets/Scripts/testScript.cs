5320490563c4812944ba05385a03091b281b74ab
ange.clement@etu.umontpellier.fr <ange.clement@etu.umontpellier.fr>
2022-10-11 17:20:03 +0200

    ajout scene Ange

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -13,6 +13,6 @@ public class testScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.1f, 0.0f, 0.0f);
+        transform.Rotate(0.0f, 0.0f, 0.1f);
     }
 }
