dbc592cc1779d56e5be7d206fa533f7f1e842dcc
ange-clement <47100137+ange-clement@users.noreply.github.com>
2022-10-23 16:22:56 +0200

    Robot

--- Assets/Scripts/Actionable/PathMovement.cs
+++ Assets/Scripts/Actionable/PathMovement.cs
@@ -10,6 +10,7 @@ public class PathMovement : Actionable
     public float minSpeed = 0.2f;
     public float decelDist = 2.0f;
     public float acceleration = 4.0f;
+    public float maxDegreesDelta = 180f;
     public bool isActivated = false;
 
 
@@ -18,8 +19,6 @@ public class PathMovement : Actionable
     public float currentDecelDist;
 
     private float speed = 0f;
-
-    private PlayerMovement player;
 
     public Vector3 ObjectSpeed { get => objectSpeed; }
     private Vector3 objectSpeed;
@@ -55,10 +54,6 @@ public class PathMovement : Actionable
         }
     }
 
-    private void Start()
-    {
-        player = FindObjectOfType<PlayerMovement>();
-    }
 
     void Update()
     {
@@ -71,6 +66,8 @@ public class PathMovement : Actionable
 
             Vector3 direction = target.position - transform.position;
             float magnitude = direction.magnitude;
+            float rotationAngle = Vector3.Angle(transform.forward, target.forward);
+            bool pass = true;
             if (magnitude > 0.05f)
             {
                 if (magnitude > currentDecelDist)
@@ -90,9 +87,17 @@ public class PathMovement : Actionable
                 }
 
                 objectSpeed = direction / magnitude * speed;
-                transform.Translate(objectSpeed * Time.deltaTime);
+                transform.position += objectSpeed * Time.deltaTime;
+
+                pass = false;
             }
-            else {
+            if (rotationAngle > 5f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, maxDegreesDelta * Time.deltaTime);
+                pass = false;
+            }
+            if (pass)
+            {
                 currentTarget = (currentTarget + 1) % pathObject.childCount;
                 UpdateTarget();
             }
